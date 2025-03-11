0b735c082589d2a2b370f394ac67983c2fdaf030
Wesley Cymerman <112433667+wesleycym@users.noreply.github.com>
2025-03-10 23:15:47 -0400

    Delete api directory

--- api/routes/productRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { addProduct } = require('../controllers/productController');
-
-router.post('/add-product', addProduct);
-
-module.exports = router;
