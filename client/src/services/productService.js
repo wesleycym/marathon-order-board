24a457a8898e0326159bb7e55dd40ef24f69fdc6
Wesley Cymerman <112433667+wesleycym@users.noreply.github.com>
2025-03-10 23:15:57 -0400

    Delete client directory

--- client/src/services/productService.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const addProduct = async (productData) => {
-    const response = await axios.post('/api/products/add-product', productData);
-    return response.data;
-};
