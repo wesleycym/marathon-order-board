de6e5d0fe3c24f2da5f9a945a52ff404d20651b1
wesleycym <wesleycy@buffalo.edu>
2025-03-20 21:24:08 -0400

    Tape Option skeleton

---
+++ frontend/src/components/Drain Options/TapeOption.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function TapeOption({}) {
+
+    return (
+        <div>
+            {/* Add your logic here */}
+        </div>
+    );
+}
+
+export default TapeOption;
