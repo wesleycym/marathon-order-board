a7efde1642fb49e9640218b19fd68c54b68a446c
wesleycym <wesleycy@buffalo.edu>
2025-03-20 21:23:45 -0400

    Seal Option skeleton

---
+++ frontend/src/components/Drain Options/SealOption.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function SealOption({}) {
+
+    return (
+        <div>
+            {/* Add your logic here */}
+        </div>
+    );
+}
+
+export default SealOption;
