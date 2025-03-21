011ed43bed9803217968fec96a7c876c721e8fba
wesleycym <wesleycy@buffalo.edu>
2025-03-20 21:14:19 -0400

    Skeleton for dome type

---
+++ frontend/src/components/Drain Options/DomeType.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function DomeType({}) {
+
+    return (
+        <div>
+            {/* Add your logic here */}
+        </div>
+    );
+}
+
+export default DomeType;
