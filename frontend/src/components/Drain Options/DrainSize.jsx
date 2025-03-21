b9e4f469b4047c1d22941fc2740a32d421c492a1
wesleycym <wesleycy@buffalo.edu>
2025-03-20 21:15:08 -0400

    Skeleton for drain size

---
+++ frontend/src/components/Drain Options/DrainSize.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function DrainSize({}) {
+
+    return (
+        <div>
+            {/* Add your logic here */}
+        </div>
+    );
+}
+
+export default DrainSize;
