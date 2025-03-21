5c0404bef69076dc2a2d017e97e54f08e89c9549
wesleycym <wesleycy@buffalo.edu>
2025-03-20 21:37:49 -0400

    Implemented Boxtype component

--- frontend/src/components/Drain Options/BoxType.jsx
+++ frontend/src/components/Drain Options/BoxType.jsx
@@ -1,10 +1,48 @@
 import React, { useState } from 'react';
 
-function BoxType({}) {
+function BoxType({selectedBox, setSelectedBox}) {
+    
+    // Box Options
+    const boxTypes = ['Aluminator', 
+        'Marathon', 
+        'TruFast',
+        'MuleHide', 
+        'ABC Catalog', 
+        'TopShield', 
+        'Hytech',
+        'IB', 
+        'Garlock', 
+        'Plain',
+        'Other...'
+    ];
 
     return (
         <div>
-            {/* Add your logic here */}
+
+            {/* Box Types */}
+            {boxTypes.map((box) => (
+                <label key = {box}>
+                    <input
+                        type = "radio"
+                        name = "boxType"
+                        value = {box}
+                        checked = {selectedBox === box}
+                        onChange = {(e) => setSelectedBox(e.target.value)}
+                    />
+                    {box}
+                </label>
+            ))}
+
+            {/* If 'Other' box type is selected */ }
+            {selectedBox === 'Other...' && (
+                <input
+                    type = "text"
+                    placeholder = "Other..."
+                    value = {selectedBox}
+                    onChange = {(e) => setSelectedBox(e.target.value)}
+                />
+            )}
+
         </div>
     );
 }
