d88a1bb1115178d2daa426aa6d92d5ce11ae6fc6
wesleycym <wesleycy@buffalo.edu>
2025-03-20 21:14:44 -0400

    Skeleton for drain amount

---
+++ frontend/src/components/Drain Options/DrainAmount.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function DrainAmount({}) {
+
+    return (
+        <div>
+            {/* Add your logic here */}
+        </div>
+    );
+}
+
+export default DrainAmount;
