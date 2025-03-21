6dcc844cf3e66db082c1f13a1286fe860f0d501b
wesleycym <wesleycy@buffalo.edu>
2025-03-21 17:51:05 -0400

    Cleaned up form

--- frontend/src/components/OrderFormDrains.jsx
+++ frontend/src/components/OrderFormDrains.jsx
@@ -1,26 +1,16 @@
 import React, { useState } from 'react';
 
-
 function OrderFormDrains({}) {
 
-
+    // Drain Options -> Keeping track of inputs
     const [selectedBox, setSelectedBox] = useState(''); // Tracking the selected box
     const [drainSize, setDrainSize] = useState(''); // Tracking Drain Size 
     const [amount, setAmount] = useState(''); // Tracking the amount
-    const [selectedSeal, setSelectedSeal] = useState({ // Seal type selection
-        ProSeal: false,
-        MaxxFlo: false
-    });
+    const [selectedSeal, setSelectedSeal] = useState({ProSeal: false, MaxxFlo: false}); // Seal type selection
     const [selectedDome, setSelectedDome] = useState(''); // Tracking the selected dome
     const [selectedRing, setSelectedRing] = useState(''); // Tracking the selected ring
-    const [selectedCoatings, setSelectedCoatings] = useState({ // Optional Coating type selection
-        TPO: false,
-        PVC: false,
-        Aslphalt: false
-    });
-    const [tapeOptions, setTapeOptions] = useState({ // Optional Tape selection
-        tape: false
-    });
+    const [selectedCoatings, setSelectedCoatings] = useState({TPO: false, PVC: false, Aslphalt: false}); // Optional Coating type selection
+    const [tapeOptions, setTapeOptions] = useState({tape: false}); // Optional Tape selection
 
     return (
         <div className = "drainType">
@@ -35,38 +25,19 @@ function OrderFormDrains({}) {
 
             {/* Box Types */}
             <h4>Box Types</h4>
-
-
-
             {/* Seal Options*/}
             <h4>Seal Options</h4>
-
-
-
             {/* Dome Types */}
             <h4> Dome Types </h4>
-
-
-
             {/* Ring Types */}
             <h4> Ring Types </h4>
-
-
-
             {/* Coating Types */}
             <h4> Coating Types </h4>
-
-
-
             {/* Tape Options */}
             <h4> Tape Options </h4>
-
-
-
+            {/* Add to Order Button */}
             <h5> Add to Order </h5>
 
-
-            
             </div>
         </div>
     );
