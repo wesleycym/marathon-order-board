6cebc454d153ae4fd9fd54ed091b5b63577d9631
wesleycym <wesleycy@buffalo.edu>
2025-03-21 17:16:12 -0400

    Coating type logic

--- frontend/src/components/Drain Options/CoatingType.jsx
+++ frontend/src/components/Drain Options/CoatingType.jsx
@@ -1,10 +1,25 @@
 import React, { useState } from 'react';
 
-function CoatingType({}) {
+function CoatingType({selectedCoatings, setSelectedCoatings}) {
 
     return (
         <div>
-            {/* Add your logic here */}
+            {Object.keys(selectedCoatings).map((coating) => (
+                <label key = {coating}>
+                    <input
+                        type = "radio"
+                        name = "coatingOptions"
+                        checked = {selectedCoatings[coating]}
+                        onChange = {() => setSelectedCoatings({
+                            TPO: false, 
+                            PVC: false,
+                            Asphalt: false,
+                            [coating]: true // Set the selected coating to be true
+                        })}
+                    />
+                    {coating}
+                </label>
+            ))}
         </div>
     );
 }
