3bbe6ba0b3653f0b0895dd22166533f0a86f2d0a
wesleycym <wesleycy@buffalo.edu>
2025-03-21 17:18:19 -0400

    Dome type logic

--- frontend/src/components/Drain Options/DomeType.jsx
+++ frontend/src/components/Drain Options/DomeType.jsx
@@ -1,10 +1,39 @@
 import React, { useState } from 'react';
 
-function DomeType({}) {
+function DomeType({selectedDome, setSelectedDome}) {
+
+    const domeTypes = [
+        'Aluminum Dome',
+        'Black Plastic Dome',
+        'Blue Plastic Dome',
+        'Other...'  
+    ];
 
     return (
         <div>
-            {/* Add your logic here */}
+            
+            {domeTypes.map((dome) => (
+                <label key = {dome}>
+                    <input
+                        type = "radio"
+                        name = "domeType"
+                        value = {dome}
+                        checked = {selectedDome === dome}
+                        onChange = {(e) => setSelectedDome(e.target.value)}
+                    />
+                    {dome}
+                </label>
+            ))}
+            {/* If 'Other' dome type is selected */ }
+            {selectedDome === 'Other...' && (
+                <input
+                    type = 'text'
+                    placeholder = 'Enter dome type'
+                    value = {selectedDome === 'Other...' ? selectedDome : ''}
+                    onChange = {(e) => setSelectedDome(e.target.value)}
+                />
+            )}
+
         </div>
     );
 }
