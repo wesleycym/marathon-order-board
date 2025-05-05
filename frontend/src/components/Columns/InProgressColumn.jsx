be99ae394f27f914e5172742e2e3eee53af0a288
wesleycym <wesleycy@buffalo.edu>
2025-05-04 20:51:16 -0400

    Created inProgress column as a seperate module

--- frontend/src/components/Columns/InProgressColumn.jsx
+++ frontend/src/components/Columns/InProgressColumn.jsx
@@ -1,2 +1,38 @@
 import { Droppable, Draggable } from '@hello-pangea/dnd';
 import './styles/columnStyling.css'
+
+function InProgressColumn({ orders }) {
+    return (
+      <Droppable droppableId="inProgress">
+        {(provided) => (
+          <div
+            className="columnInProgress"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <h2>In Progress</h2>
+  
+            {orders.map((order, index) => (
+              <Draggable key={order.orderNumber} draggableId={order.orderNumber} index={index}>
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    className="draggable-order"
+                  >
+                    {order.orderNumber}
+                  </div>
+                )}
+              </Draggable>
+            ))}
+  
+            {provided.placeholder}
+
+          </div>
+        )}
+      </Droppable>
+    );
+  }
+  
+  export default InProgressColumn;
