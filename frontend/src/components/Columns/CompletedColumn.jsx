b4b20c587fb49bde9a8df7f17e98c44273d38a00
wesleycym <wesleycy@buffalo.edu>
2025-05-04 21:04:09 -0400

    completedColumn modularized

--- frontend/src/components/Columns/CompletedColumn.jsx
+++ frontend/src/components/Columns/CompletedColumn.jsx
@@ -1,2 +1,38 @@
 import { Droppable, Draggable } from '@hello-pangea/dnd';
 import './styles/columnStyling.css'
+
+function CompletedColumn({ orders }) {
+    return (
+      <Droppable droppableId="completed">
+        {(provided) => (
+          <div
+            className="columnCompleted"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <h2>Review</h2>
+  
+            {orders.map((order, index) => (
+              <Draggable key={order.orderNumber} draggableId={order.orderNumber} index={index}>
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    className="draggable-order"
+                  >
+                    {order.orderNumber}
+                  </div>
+                )}
+              </Draggable>
+            ))}
+  
+            {provided.placeholder}
+
+          </div>
+        )}
+      </Droppable>
+    );
+  }
+  
+  export default CompletedColumn;
