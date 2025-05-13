a92e2a57bbeee19593e51fc3912e11b524d4ae16
wesleycym <wesleycy@buffalo.edu>
2025-05-13 12:52:19 -0400

    Created empty function for OrderCard.jsx

--- frontend/src/components/OrderCardComponents/OrderCard.jsx
+++ frontend/src/components/OrderCardComponents/OrderCard.jsx
@@ -1,1 +1,12 @@
 // Main card layout
+import { useState } from 'react'
+
+function OrderCard({}) {
+    return (
+        <div>
+            <p>This is an order</p>
+        </div>
+    );
+  }
+  
+  export default OrderCard;
