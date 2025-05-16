7cc3c0f08f2002d6f0ce8a475b22604969ef8d01
wesleycym <wesleycy@buffalo.edu>
2025-05-16 15:36:59 -0400

    Created simple card logic

--- frontend/src/components/OrderCardComponents/OrderCard.jsx
+++ frontend/src/components/OrderCardComponents/OrderCard.jsx
@@ -3,8 +3,9 @@ import { useState } from 'react'
 
 function OrderCard({order}) {
     return (
-        <div className="order-card">
+        <div className="w-[95%] mx-auto p-2 rounded-md shadow-md border">
             <p>Order #: {order.orderNumber}</p>
+            <p>Type: {[order.Box]}</p>
         </div>
     );
   }
