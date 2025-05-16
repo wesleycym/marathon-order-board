ca0d9e15417319a21b65863575ef8b0ecd08e751
wesleycym <wesleycy@buffalo.edu>
2025-05-16 15:38:06 -0400

    Fixed scaling in completed column

--- frontend/src/components/Columns/CompletedColumn.jsx
+++ frontend/src/components/Columns/CompletedColumn.jsx
@@ -6,30 +6,31 @@ function CompletedColumn({ orders }) {
     return (
       <Droppable droppableId="completed">
         {(provided) => (
-          <div
-            className="columnCompleted"
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-          >
+          <div className="columnCompleted flex flex-col h-full">
+            
             <h2>Completed</h2>
   
-            {orders.map((order, index) => (
-              <Draggable key={order.orderNumber} draggableId={order.orderNumber} index={index}>
-                {(provided) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    className="draggable-order"
-                  >
-                    <OrderCard order = {order}/> {/* Order card component */}
-                  </div>
-                )}
-              </Draggable>
-            ))}
-  
-            {provided.placeholder}
-
+            <div 
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="flex-1 flex flex-col justify-start min-h-[10rem]"
+            >
+              {orders.map((order, index) => (
+                <Draggable key={order.orderNumber} draggableId={order.orderNumber} index={index}>
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className="draggable-order py-2"
+                    >
+                      <OrderCard order = {order}/> {/* Order card component */}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
           </div>
         )}
       </Droppable>
