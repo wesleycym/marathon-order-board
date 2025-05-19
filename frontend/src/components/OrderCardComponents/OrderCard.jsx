7c9de42289b6135ef00512e2562e7fe4c1783629
wesleycym <wesleycy@buffalo.edu>
2025-05-19 10:41:17 -0400

    Card colors implemented

--- frontend/src/components/OrderCardComponents/OrderCard.jsx
+++ frontend/src/components/OrderCardComponents/OrderCard.jsx
@@ -4,10 +4,15 @@ import { useState } from 'react'
 import { boxTypeColors } from './OrderCardStyling';
 
 function OrderCard({order}) {
+
+    const firstBoxType = Object.values(order.drains)[0]?.box || 'Plain'; // Pulling the first box type from the order -> Maybe change to pull the most common box type
+    const bgClass = boxTypeColors[firstBoxType] || 'bg-white';
+
     return (
-        <div className="w-[95%] mx-auto p-2 rounded-md shadow-md border">
-            <p>Order #: {order.orderNumber}</p>
-            <p>Ship Date: {order.orderDate}</p>
+        <div className={`w-[95%] mx-auto p-4 rounded-md shadow-md border transition-all duration-150 ${bgClass}`}>
+        <h3 className="text-lg font-bold">Order #{order.orderNumber}</h3>
+        <p className="text-sm text-gray-800">{Object.keys(order.drains).length} drain(s)</p>
+        <p className="text-xs italic">Ship Date: {order.shipDate}</p>
         </div>
     );
   }
