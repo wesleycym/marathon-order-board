86a1516b9359c9d7ebb2c8f76420352cb5644d46
wesleycym <wesleycy@buffalo.edu>
2025-05-19 10:24:07 -0400

    Added orderDate to OrderCard

--- frontend/src/components/OrderCardComponents/OrderCard.jsx
+++ frontend/src/components/OrderCardComponents/OrderCard.jsx
@@ -5,7 +5,7 @@ function OrderCard({order}) {
     return (
         <div className="w-[95%] mx-auto p-2 rounded-md shadow-md border">
             <p>Order #: {order.orderNumber}</p>
-            <p>Type: {[order.Box]}</p>
+            <p>Ship Date: {order.orderDate}</p>
         </div>
     );
   }
