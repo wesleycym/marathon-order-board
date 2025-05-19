e7392287e2e8cb738cae1c1161a30002f2c4c5bc
wesleycym <wesleycy@buffalo.edu>
2025-05-19 10:28:31 -0400

    Created mapping for box type to color

---
+++ frontend/src/components/OrderCardComponents/OrderCardStyling.jsx
@@ -0,0 +1,13 @@
+// File for mapping box types to colors
+
+export const boxTypeColors = {
+  Marathon: 'bg-orange-400',
+  MuleHide: 'bg-purple-400',
+  TopShield: 'bg-yellow-400',
+  Aluminator: 'bg-blue-200 text-blue-800',
+  IB: 'bg-white border-2 border-blue-300 text-blue-700',
+  Hytech: 'bg-stone-300 text-brown-900',
+  TruFast: 'bg-blue-600 text-white',
+  Plain: 'bg-gray-100 text-black',
+  Garlock: 'bg-pink-400',
+};
