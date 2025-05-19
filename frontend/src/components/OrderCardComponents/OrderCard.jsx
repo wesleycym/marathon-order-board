db1aa1e552d3b4d2fcde44454179c199e4e0be1a
wesleycym <wesleycy@buffalo.edu>
2025-05-19 10:48:49 -0400

    Calculating total amount of drains; Fixed orderDate

--- frontend/src/components/OrderCardComponents/OrderCard.jsx
+++ frontend/src/components/OrderCardComponents/OrderCard.jsx
@@ -5,14 +5,21 @@ import { boxTypeColors } from './OrderCardStyling';
 
 function OrderCard({order}) {
 
+    // Determine the background color
     const firstBoxType = Object.values(order.drains)[0]?.box || 'Plain'; // Pulling the first box type from the order -> Maybe change to pull the most common box type
     const bgClass = boxTypeColors[firstBoxType] || 'bg-white';
 
+    // Determine the quantity of all drains
+    const totalDrains = Object.values(order.drains).reduce((sum, drain) => {
+        const amount = parseInt(drain.total, 10);
+        return sum + (isNaN(amount) ? 0 : amount);
+        }, 0);
+
     return (
         <div className={`w-[95%] mx-auto p-4 rounded-md shadow-md border transition-all duration-150 ${bgClass}`}>
-        <h3 className="text-lg font-bold">Order #{order.orderNumber}</h3>
-        <p className="text-sm text-gray-800">{Object.keys(order.drains).length} drain(s)</p>
-        <p className="text-xs italic">Ship Date: {order.shipDate}</p>
+            <h3 className="text-lg font-bold">Order #{order.orderNumber}</h3>
+            <p className="text-sm text-gray-800">{totalDrains} Drain(s)</p>
+            <p className="text-xs italic">Ship Date: {order.orderDate}</p>
         </div>
     );
   }
