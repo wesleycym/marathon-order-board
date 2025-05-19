9030041b8532fac5de47af1c09da793ba9b3755c
wesleycym <wesleycy@buffalo.edu>
2025-05-19 10:30:09 -0400

    Imported card styling

--- frontend/src/components/OrderCardComponents/OrderCard.jsx
+++ frontend/src/components/OrderCardComponents/OrderCard.jsx
@@ -1,6 +1,8 @@
 // Main card layout
 import { useState } from 'react'
 
+import { boxTypeColors } from './OrderCardStyling';
+
 function OrderCard({order}) {
     return (
         <div className="w-[95%] mx-auto p-2 rounded-md shadow-md border">
