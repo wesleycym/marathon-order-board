ccf2c3454fcc366d7c09f6c6d9dd865d3ad94f5b
wesleycym <wesleycy@buffalo.edu>
2025-05-21 16:16:26 -0400

    Changed color to aluminator

--- frontend/src/components/OrderCardComponents/OrderCardStyling.jsx
+++ frontend/src/components/OrderCardComponents/OrderCardStyling.jsx
@@ -4,7 +4,7 @@ export const boxTypeColors = {
   Marathon: 'bg-orange-400',
   MuleHide: 'bg-green-500',
   TopShield: 'bg-yellow-400',
-  Aluminator: 'bg-blue-200 text-blue-800',
+  Aluminator: 'bg-white text-blue-800',
   IB: 'bg-white border-2 border-blue-300 text-blue-700',
   Hytech: 'bg-amber-950',
   TruFast: 'bg-blue-600 text-white',
